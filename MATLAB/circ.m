4a7ef05553ad2bbca911263c5859dffacb3727c0
Ben <simons.ben0@gmail.com>
2016-12-03 18:24:05 -0500

    Published code and comments

--- MATLAB/circ.m
+++ MATLAB/circ.m
@@ -1,19 +1,24 @@
 function [pts] = circ(cent, rad)
     %Function plots circle that can be used for inpolygon function
 
+    %Generating initial x coordinates
     x = [linspace(-rad, rad) linspace(rad, -rad)];
     initl = length(x);
 
+    %Generating positive y coordinates
     for i = 1:length(x)/2
         y(i) = sqrt(rad^2 - x(i)^2) + cent(2);
     end
+    %Generating negative y coordinates
     for i = length(x)/2:length(x)
         y(i) = -sqrt(rad^2 - x(i)^2) + cent(2);
     end
+    %Adjusting x coordinates based on center
     for i = 1:length(x)
         x(i) = x(i) + cent(1);
     end
     
+    %Outputting points
     pts(:, 1) = x';
     pts(:, 2) = y';
 end
